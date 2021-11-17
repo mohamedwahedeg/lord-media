7d408ab5e15bea8f983043408f3489520eeff595
mohamedwahedeg <90778952+mohamedwahedeg@users.noreply.github.com>
2021-11-17 09:47:52 +0200

    cdn files v1
    upload cdn files v1

---
+++ cdn/lord-media-click-protection.js
@@ -0,0 +1,60 @@
+(function() {
+var script = document.createElement('script');
+script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+document.writeln('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>');
+document.writeln('<script type="text/javascript" src="https://lord-media.com/cdn/iframeTracker-jquery.js"></script>');
+
+$( document ).ready(function() {
+
+$('#gpt-passback,#gpt-passback-top,#gpt-passback-before-content,#gpt-passback-in-content1,#gpt-passback-in-content2,#gpt-passback-after-content,#gpt-passback-sidebar').iframeTracker(function(event) {
+// check cookie //
+  let cookie0 = getCookie("lordclicked");
+  if (cookie0 != "") {
+  var integer0 = parseInt(cookie0, 10);
+  let lcv1 = integer0 + 1;
+const d = new Date();
+d.setTime(d.getTime() + (6*60*60*1000));
+let expires0 = "expires="+ d.toUTCString();
+document.cookie = "lordclicked" + "=" + lcv1 + ";" + expires0 + ";path=/";
+  } else {
+let lcv = 1;
+const d = new Date();
+d.setTime(d.getTime() + (6*60*60*1000));
+let expires1 = "expires="+ d.toUTCString();
+document.cookie = "lordclicked" + "=" + lcv + ";" + expires1 + ";path=/";
+  }
+  });
+    
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+setTimeout(function() {
+  let lordcookie0 = getCookie("lordclicked");
+  var lordcookie1 = parseInt(lordcookie0, 10);
+  if(lordcookie1 >= 5){
+    lordhideadunit();
+  }
+}, 1000);
+
+  function lordhideadunit() {
+    $('#gpt-passback,#gpt-passback-top,#gpt-passback-before-content,#gpt-passback-in-content1,#gpt-passback-in-content2,#gpt-passback-after-content,#gpt-passback-sidebar').hide();
+	}
+    
+});
+
+   
+})();
---
+++ cdn/tags.js
@@ -0,0 +1,302 @@
+let url_string = window.location;
+let url = new URL(url_string);
+let w = url.searchParams.get("w");
+console.log(w);
+// start writing //
+document.writeln("<center> <h1> tags for "+ w +" website </h1> </center>");
+document.writeln("<br>");
+document.writeln("<h3> first integration code , put this block inside &lt;head&gt; area in your website. </h3> ");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln("&lt;link href='//lord-media.com' rel='dns-prefetch'/&gt;");
+document.writeln("<br>");
+document.writeln('&lt;link rel="stylesheet" type="text/css" href="https://lord-media.com/cdn/style-lord-media.css"&gt;');
+document.writeln("<br>");
+document.writeln('&lt;link href="//lord-media.com/cdn/websites/'+ w +'.js" rel="preload"/&gt;');
+document.writeln("<br>");
+document.writeln('&lt;script src="https://lord-media.com/cdn/websites/'+ w +'.js"&gt;&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln("&lt;script async src='https://securepubads.g.doubleclick.net/tag/js/gpt.js'&gt;&lt;/script&gt;");
+
+document.writeln("<br>");
+document.writeln("<br>");
+
+
+
+
+
+
+document.writeln("<h3> and put this code in footer of your website. </h3> ");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln('&lt;script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"&gt;&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;div id="gpt-passback-integeration"&gt;');
+document.writeln("<br>");
+document.writeln('&lt;script&gt;');
+document.writeln("<br>");
+document.writeln('window.googletag = window.googletag || {cmd: []};');
+document.writeln("<br>");
+document.writeln('googletag.cmd.push(function() {');
+document.writeln("<br>");
+document.writeln("googletag.defineSlot('/22561025441/"+ w +"/integeration', [1, 1], 'gpt-passback-integeration').addService(googletag.pubads());");
+document.writeln("<br>");
+document.writeln('googletag.enableServices();');
+document.writeln("<br>");
+document.writeln("googletag.display('gpt-passback-integeration');");
+document.writeln("<br>");
+document.writeln('});');
+document.writeln("<br>");
+document.writeln('&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;/div&gt;');
+document.writeln("<br>");
+document.writeln("<br>");
+
+
+
+document.writeln("<hr>");
+
+document.writeln("<br>");
+document.writeln("<h3> second Ads Units codes , put them in place you want in your website. </h3>");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln("<h4> ad tag for top of pages. </h4>");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln('&lt;script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"&gt;&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;div id="gpt-passback-top"&gt;');
+document.writeln("<br>");
+document.writeln('&lt;script&gt;');
+document.writeln("<br>");
+document.writeln('window.googletag = window.googletag || {cmd: []};');
+document.writeln("<br>");
+document.writeln('googletag.cmd.push(function() {');
+document.writeln("<br>");
+document.writeln("googletag.defineSlot('/22561025441/"+ w +"', [[125, 125], [250, 250], [336, 280], [320, 50], [468, 60], [200, 200], [300, 250], [320, 480]], 'gpt-passback-top').addService(googletag.pubads());");
+document.writeln("<br>");
+document.writeln('googletag.enableServices();');
+document.writeln("<br>");
+document.writeln("googletag.display('gpt-passback-top');");
+document.writeln("<br>");
+document.writeln('});');
+document.writeln("<br>");
+document.writeln('&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;/div&gt;');
+document.writeln("<br>");
+
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("--------------------");
+
+
+
+
+document.writeln("<h4> ad tag for before content. </h4>");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln('&lt;script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"&gt;&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;div id="gpt-passback-before-content"&gt;');
+document.writeln("<br>");
+document.writeln('&lt;script&gt;');
+document.writeln("<br>");
+document.writeln('window.googletag = window.googletag || {cmd: []};');
+document.writeln("<br>");
+document.writeln('googletag.cmd.push(function() {');
+document.writeln("<br>");
+document.writeln("googletag.defineSlot('/22561025441/"+ w +"', [[125, 125], [250, 250], [336, 280], [320, 50], [468, 60], [200, 200], [300, 250], [320, 480]], 'gpt-passback-before-content').addService(googletag.pubads());");
+document.writeln("<br>");
+document.writeln('googletag.enableServices();');
+document.writeln("<br>");
+document.writeln("googletag.display('gpt-passback-before-content');");
+document.writeln("<br>");
+document.writeln('});');
+document.writeln("<br>");
+document.writeln('&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;/div&gt;');
+document.writeln("<br>");
+
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("--------------------");
+
+
+
+
+
+document.writeln("<h4> ad tag for in content1. </h4>");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln('&lt;script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"&gt;&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;div id="gpt-passback-in-content1"&gt;');
+document.writeln("<br>");
+document.writeln('&lt;script&gt;');
+document.writeln("<br>");
+document.writeln('window.googletag = window.googletag || {cmd: []};');
+document.writeln("<br>");
+document.writeln('googletag.cmd.push(function() {');
+document.writeln("<br>");
+document.writeln("googletag.defineSlot('/22561025441/"+ w +"', [[125, 125], [250, 250], [336, 280], [320, 50], [468, 60], [200, 200], [300, 250], [320, 480]], 'gpt-passback-in-content1').addService(googletag.pubads());");
+document.writeln("<br>");
+document.writeln('googletag.enableServices();');
+document.writeln("<br>");
+document.writeln("googletag.display('gpt-passback-in-content1');");
+document.writeln("<br>");
+document.writeln('});');
+document.writeln("<br>");
+document.writeln('&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;/div&gt;');
+document.writeln("<br>");
+
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("--------------------");
+
+
+
+
+document.writeln("<h4> ad tag for in content2. </h4>");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln('&lt;script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"&gt;&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;div id="gpt-passback-in-content2"&gt;');
+document.writeln("<br>");
+document.writeln('&lt;script&gt;');
+document.writeln("<br>");
+document.writeln('window.googletag = window.googletag || {cmd: []};');
+document.writeln("<br>");
+document.writeln('googletag.cmd.push(function() {');
+document.writeln("<br>");
+document.writeln("googletag.defineSlot('/22561025441/"+ w +"', [[125, 125], [250, 250], [336, 280], [320, 50], [468, 60], [200, 200], [300, 250], [320, 480]], 'gpt-passback-in-content2').addService(googletag.pubads());");
+document.writeln("<br>");
+document.writeln('googletag.enableServices();');
+document.writeln("<br>");
+document.writeln("googletag.display('gpt-passback-in-content2');");
+document.writeln("<br>");
+document.writeln('});');
+document.writeln("<br>");
+document.writeln('&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;/div&gt;');
+document.writeln("<br>");
+
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("--------------------");
+
+
+
+
+
+document.writeln("<h4> ad tag for after content. </h4>");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln('&lt;script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"&gt;&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;div id="gpt-passback-after-content"&gt;');
+document.writeln("<br>");
+document.writeln('&lt;script&gt;');
+document.writeln("<br>");
+document.writeln('window.googletag = window.googletag || {cmd: []};');
+document.writeln("<br>");
+document.writeln('googletag.cmd.push(function() {');
+document.writeln("<br>");
+document.writeln("googletag.defineSlot('/22561025441/"+ w +"', [[125, 125], [250, 250], [336, 280], [320, 50], [468, 60], [200, 200], [300, 250], [320, 480]], 'gpt-passback-after-content').addService(googletag.pubads());");
+document.writeln("<br>");
+document.writeln('googletag.enableServices();');
+document.writeln("<br>");
+document.writeln("googletag.display('gpt-passback-after-content');");
+document.writeln("<br>");
+document.writeln('});');
+document.writeln("<br>");
+document.writeln('&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;/div&gt;');
+document.writeln("<br>");
+
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("--------------------");
+
+
+
+
+
+document.writeln("<h4> ad tag for sidebar. </h4>");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln('&lt;script async src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"&gt;&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;div id="gpt-passback-sidebar"&gt;');
+document.writeln("<br>");
+document.writeln('&lt;script&gt;');
+document.writeln("<br>");
+document.writeln('window.googletag = window.googletag || {cmd: []};');
+document.writeln("<br>");
+document.writeln('googletag.cmd.push(function() {');
+document.writeln("<br>");
+document.writeln("googletag.defineSlot('/22561025441/"+ w +"', [[125, 125], [250, 250], [336, 280], [320, 50], [468, 60], [200, 200], [300, 250], [320, 480]], 'gpt-passback-sidebar').addService(googletag.pubads());");
+document.writeln("<br>");
+document.writeln('googletag.enableServices();');
+document.writeln("<br>");
+document.writeln("googletag.display('gpt-passback-sidebar');");
+document.writeln("<br>");
+document.writeln('});');
+document.writeln("<br>");
+document.writeln('&lt;/script&gt;');
+document.writeln("<br>");
+document.writeln('&lt;/div&gt;');
+document.writeln("<br>");
+
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("--------------------");
+
+
+
+
+
+
+document.writeln("<br>");
+
+document.writeln("<h4> video tag </h4>");
+document.writeln("<br>");
+document.writeln("<br>");
+
+document.writeln('&lt;div id="protag-in_article_video"&gt;&lt;/div&gt;');
+document.writeln("<br>");
+document.writeln('&lt;script type="text/javascript"&gt;');
+document.writeln("<br>");
+document.writeln('window.googletag = window.googletag || { cmd: [] };');
+document.writeln("<br>");
+document.writeln('window.protag = window.protag || { cmd: [] };');
+document.writeln("<br>");
+document.writeln('window.protag.cmd.push(function () {');
+document.writeln("<br>");
+document.writeln('window.protag.display("protag-in_article_video");');
+document.writeln("<br>");
+document.writeln('});');
+document.writeln("<br>");
+document.writeln('&lt;/script&gt;');
+document.writeln("<br>");
+
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("--------------------");
+
