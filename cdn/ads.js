75f7bbef6dc811e3da026cddf55dab47b493ae2f
mohamedwahedeg <90778952+mohamedwahedeg@users.noreply.github.com>
2021-11-17 10:43:21 +0200

    update adsjs file - adding getting website from url

--- cdn/ads.js
+++ cdn/ads.js
@@ -1,16 +1,21 @@
+let url_string = window.location;
+let url = new URL(url_string);
+let w = url.searchParams.get("w");
+console.log(w);
+
 var scriptGPT = document.createElement('script');scriptGPT.setAttribute("async", "async");
 scriptGPT.src = 'https://securepubads.g.doubleclick.net/tag/js/gpt.js';
 scriptGPT.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(scriptGPT);
 
 document.writeln('<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>');
-document.writeln('<script type="text/javascript" src="https://lord-media.com/cdn/lord-media-click-protection.js"></script>');
+document.writeln('<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/mohamedwahedeg/lord-media/cdn/lord-media-click-protection.js"></script>');
 
 // start lord media interstitial ads //
       window.googletag = window.googletag || {cmd: []};
       var interstitialSlot, staticSlot;
       googletag.cmd.push(function() {
-interstitialSlot = googletag.defineOutOfPageSlot( '/22561025441/egy4ever.com', googletag.enums.OutOfPageFormat.INTERSTITIAL);
+interstitialSlot = googletag.defineOutOfPageSlot( '/22561025441/'+ w +', googletag.enums.OutOfPageFormat.INTERSTITIAL);
       if (interstitialSlot) {
           interstitialSlot.addService(googletag.pubads());
           googletag.pubads().addEventListener('slotOnload', function(event) {
@@ -20,7 +25,7 @@ interstitialSlot = googletag.defineOutOfPageSlot( '/22561025441/egy4ever.com', g
           });
          console.log(interstitialSlot);
         }
-        staticSlot = googletag.defineSlot('/22561025441/egy4ever.com', [[300, 250], [336, 280]], 'static-ad-1').addService(googletag.pubads());
+        staticSlot = googletag.defineSlot('/22561025441/'+ w +', [[300, 250], [336, 280]], 'static-ad-1').addService(googletag.pubads());
         googletag.pubads().enableSingleRequest();
         googletag.enableServices();
       });
@@ -28,7 +33,6 @@ interstitialSlot = googletag.defineOutOfPageSlot( '/22561025441/egy4ever.com', g
 // end lord media interstitial ads //
 
 // start lord media anchor sticky buttom ad //
-
 
 var onscrollv = false;
 window.onload = function() {
@@ -50,7 +54,6 @@ document.getElementById('lord-media-anchor-sticky-top').innerHTML = "<div class=
 
 document.getElementById('lord-media-anchor-sticky-buttom').innerHTML = "<div class='sticky-ads-close' style='display: none;' onclick='document.getElementById(&quot;lord-media-anchor-sticky-buttom&quot;).style.display=&quot;none&quot;'><svg viewBox='0 0 512 512' xmlns='http://www.w3.org/2000/svg'><path d='M278.6 256l68.2-68.2c6.2-6.2 6.2-16.4 0-22.6-6.2-6.2-16.4-6.2-22.6 0L256 233.4l-68.2-68.2c-6.2-6.2-16.4-6.2-22.6 0-3.1 3.1-4.7 7.2-4.7 11.3 0 4.1 1.6 8.2 4.7 11.3l68.2 68.2-68.2 68.2c-3.1 3.1-4.7 7.2-4.7 11.3 0 4.1 1.6 8.2 4.7 11.3 6.2 6.2 16.4 6.2 22.6 0l68.2-68.2 68.2 68.2c6.2 6.2 16.4 6.2 22.6 0 6.2-6.2 6.2-16.4 0-22.6L278.6 256z'/></svg></div> <div id='gpt-passback-lord-media-anchor-sticky-buttom'></div>";
 
-
 googletag.cmd.push(function() {
           googletag.display(staticSlot);
 });
@@ -69,14 +72,14 @@ console.log("in mobile");
 
 window.googletag = window.googletag || {cmd: []};
     googletag.cmd.push(function() {
-    googletag.defineSlot('/22561025441/egy4ever.com', [468, 60], 'gpt-passback-lord-media-anchor-sticky-top').addService(googletag.pubads());
+    googletag.defineSlot('/22561025441/'+ w +', [468, 60], 'gpt-passback-lord-media-anchor-sticky-top').addService(googletag.pubads());
     googletag.enableServices();
     googletag.display('gpt-passback-lord-media-anchor-sticky-top');
     });
 
 window.googletag = window.googletag || {cmd: []};
     googletag.cmd.push(function() {
-    googletag.defineSlot('/22561025441/egy4ever.com', [468, 60], 'gpt-passback-lord-media-anchor-sticky-buttom').addService(googletag.pubads());
+    googletag.defineSlot('/22561025441/'+ w +', [468, 60], 'gpt-passback-lord-media-anchor-sticky-buttom').addService(googletag.pubads());
     googletag.enableServices();
     googletag.display('gpt-passback-lord-media-anchor-sticky-buttom');
     });
@@ -86,14 +89,14 @@ console.log("in web");
 
 window.googletag = window.googletag || {cmd: []};
     googletag.cmd.push(function() {
-    googletag.defineSlot('/22561025441/egy4ever.com', [[728, 90], [468, 60]], 'gpt-passback-lord-media-anchor-sticky-top').addService(googletag.pubads());
+    googletag.defineSlot('/22561025441/'+ w +', [[728, 90], [468, 60]], 'gpt-passback-lord-media-anchor-sticky-top').addService(googletag.pubads());
     googletag.enableServices();
     googletag.display('gpt-passback-lord-media-anchor-sticky-top');
     });
 
 window.googletag = window.googletag || {cmd: []};
     googletag.cmd.push(function() {
-    googletag.defineSlot('/22561025441/egy4ever.com', [[728, 90], [468, 60]], 'gpt-passback-lord-media-anchor-sticky-buttom').addService(googletag.pubads());
+    googletag.defineSlot('/22561025441/'+ w +', [[728, 90], [468, 60]], 'gpt-passback-lord-media-anchor-sticky-buttom').addService(googletag.pubads());
     googletag.enableServices();
     googletag.display('gpt-passback-lord-media-anchor-sticky-buttom');
     });
