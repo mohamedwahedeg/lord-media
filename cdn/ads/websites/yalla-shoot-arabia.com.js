0ce27bd3621795cff61e464e5eb5905c7e7467cb
mohamedwahedeg <90778952+mohamedwahedeg@users.noreply.github.com>
2022-01-25 22:57:23 +0200

    Create yalla-shoot-arabia.com.js

---
+++ cdn/ads/websites/yalla-shoot-arabia.com.js
@@ -0,0 +1,26 @@
+var scripta = document.createElement('script');
+scripta.src = 'https://securepubads.g.doubleclick.net/tag/js/gpt.js';
+scripta.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(scripta);
+
+    window.googletag = window.googletag || { cmd: [] };
+    window.googletag.cmd.push(function () {
+        window.googletag.pubads().enableSingleRequest();
+    });
+
+var scripta2 = document.createElement('script');
+scripta2.src = 'https://protagcdn.com/s/yalla-shoot-arabia.com/site.js';
+scripta2.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(scripta2);
+
+    window.protag = window.protag || { cmd: [] };
+    window.protag.config = { s:'yalla-shoot-arabia.com', childADM: '22674821885', l: 'FbM3ys2m' };
+    window.protag.cmd.push(function () {
+        window.protag.pageInit();
+    });
+
+    window.googletag = window.googletag || { cmd: [] };
+    window.protag = window.protag || { cmd: [] };
+    window.protag.cmd.push(function () {
+        window.protag.display("protag-sticky-bottom");
+    });
