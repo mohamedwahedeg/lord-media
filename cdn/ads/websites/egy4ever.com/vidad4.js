2c8407d98590731fadc4ada9eecbce1913fb4cc1
mohamedwahedeg <90778952+mohamedwahedeg@users.noreply.github.com>
2022-02-26 01:59:26 +0200

    Update vidad4.js

--- cdn/ads/websites/egy4ever.com/vidad4.js
+++ cdn/ads/websites/egy4ever.com/vidad4.js
@@ -1,7 +1,7 @@
 var div = document.getElementById("lord-media-video-ads");
 var pimg = "https://i.ibb.co/pWRvzwF/274086898-105002632117387-3534707542950503606-n.jpg";
 var vidurl = "https://www.googleapis.com/drive/v3/files/1_XbNp0VHGpfBw4BEYSbUr_Cakd2TH2_7?alt=media&key=AIzaSyAdbnd1P5YrJS5PGYeINl5aDUi-_ee0Idc";
-div.innerHTML = '<div id="videoContainer" class="videoContainer"> <div id="closevideox" style="display:none;bottom: 150px;right: 10px;position: absolute;" onclick="document.getElementById(&quot;videoContainer&quot;).style.display=&quot;none&quot;"> X </div> <video style="width:100%; height:100%;" id="mediaElement" controls playsinline poster="'+ pimg +'" preload="none" muted="muted"> <source type="video/mp4" src="'+ vidurl +'"></video> <div id="adContainer" style="position:absolute; left:0; top:0;"></div> </div>';
+div.innerHTML = '<div id="videoContainer" class="videoContainer"> <div id="closevideox" style="display:none;bottom: 250px;right: 10px;position: absolute;" onclick="document.getElementById(&quot;videoContainer&quot;).style.display=&quot;none&quot;"> X </div> <video style="width:100%; height:100%;" id="mediaElement" controls playsinline poster="'+ pimg +'" preload="none" muted="muted"> <source type="video/mp4" src="'+ vidurl +'"></video> <div id="adContainer" style="position:absolute; left:0; top:0;"></div> </div>';
 
 var adsRenderingSettings = new google.ima.AdsRenderingSettings();
       var playAdsRequest = new google.ima.AdsRequest();
